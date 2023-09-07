a8b71be5f06431f9c815140ef5f0a6bfef5acdba
Andrew Zang <andrewz020529@gmail.com>
2023-09-06 22:05:41 -0400

    created airflow locally

---
+++ airflow_local/createUser.sh
@@ -0,0 +1,7 @@
+airflow users create \
+          --username admin \
+          --firstname FIRST_NAME \
+          --lastname LAST_NAME \
+          --role Admin \
+          --email admin@example.org
+          
